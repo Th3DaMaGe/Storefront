0bf5e960e4d0bd5b4265c70eb08f1ffbf6899984
Th3DaMaGe <chikwanda.sakala@gmail.com>
2024-08-10 21:32:15 +0200

    SMTP Config

--- playground/views.py
+++ playground/views.py
@@ -1,5 +1,20 @@
 from django.shortcuts import render
+from django.core.mail import send_mail, mail_admins, BadHeaderError
 
 
 def say_hello(request):
-    return render(request, 'hello.html', {'name': 'Mosh'})
+    try:
+        send_mail("subject", "message", "perihelion@perihelion.com", ["bob@bobby.com"])
+    except BadHeaderError:
+        pass
+    return render(request, "hello.html", {"name": "Mosh"})
+
+
+def email(request):
+    try:
+        mail_admins(
+            "subject", "message", "perihelion@perihelion.com", ["bob@bobby.com"]
+        )
+    except BadHeaderError:
+        pass
+    return render(request, "hello.html", {"name": "User"})
